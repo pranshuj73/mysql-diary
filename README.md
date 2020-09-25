ad1e81d98d37c92c9cac8d1fa5e3a04aa8c2d172
volt9801 <pranshuj73@gmail.com>
2020-09-25 14:47:01 +0530

    created homescreen, diary and diary-create frames in tkinter

---
+++ README.md
@@ -0,0 +1,6 @@
+# Andy - Diary & To-Do
+Andy uses MySQL as database to store your diary entries as well as your tasks in the to-do list.
+
+
+### Credits
++ Icons made by [Freepik](http://www.freepik.com/) from [www.flaticon.com](https://www.flaticon.com/)
---
+++ andy.py
@@ -0,0 +1,132 @@
+from tkinter import *
+from tkinter.scrolledtext import ScrolledText
+from PIL import ImageTk, Image
+
+
+class Andy(Tk):
+	def __init__(self):
+		Tk.__init__(self)
+		self.minsize(500, 500)
+		self.title("Andy - Diary & To-Do")
+		container = Frame(self)
+		container.pack(side="top", fill="both", expand=True)
+		container.grid_rowconfigure(0, weight=1)
+		container.grid_columnconfigure(0, weight=1)
+
+		self.frames = {}
+		for frame_page in (Homescreen, DiaryFrame, DiaryCreateFrame):
+			page_name = frame_page.__name__
+			frame = frame_page(parent=container, controller=self)
+			self.frames[page_name] = frame
+
+			frame.grid(row=0, column=0, sticky="nsew")
+
+		self.show_frame("Homescreen")
+
+	def show_frame(self, page_name):
+		'''Show a frame for the given page name'''
+		frame = self.frames[page_name]
+		frame.tkraise()
+
+
+
+class Homescreen(Frame):
+	def __init__(self, parent, controller):
+		Frame.__init__(self, parent)
+		self.controller = controller
+
+		global andy, diary_img, todo_img
+
+		andy_img = Image.open("Andy.png").resize((313, 252), Image.ANTIALIAS)
+		andy = ImageTk.PhotoImage(andy_img)
+		image = Label(self, image=andy, text="Hii, my name is Andy, your personal diary and to-do app. ^_^", compound="top")
+		image.pack()
+
+		diary_img = ImageTk.PhotoImage(Image.open("diary.png").resize((60, 60), Image.ANTIALIAS))
+		diary_btn = Button(self, text="Diary", image=diary_img, anchor="center", height=100, width=100, compound="top", bd=0, command=lambda: controller.show_frame("DiaryFrame"))
+		diary_btn.pack(side="left", expand=True)
+
+		todo_img = ImageTk.PhotoImage(Image.open("todo.png").resize((60, 60), Image.ANTIALIAS))
+		todo_btn = Button(self, text="To-Do", image=todo_img, anchor="center", height=100, width=100, compound="top", bd=0)
+		todo_btn.pack(side="left", expand=True)
+
+
+class DiaryFrame(Frame):
+	def __init__(self, parent, controller):
+		Frame.__init__(self, parent)
+		self.controller = controller
+
+		global back_img, create_img
+
+		back_img = ImageTk.PhotoImage(Image.open("back.png"))
+		back_btn = Button(self, text="Back to Menu", image=back_img, anchor="center", compound="left", bd=0, padx=-10, command=lambda: controller.show_frame("Homescreen"))
+		back_btn.pack(pady=(0,20))
+
+		image = Label(self, image=diary_img, text="Diary", compound="left", padx=10)
+		image.pack()
+
+		diary_help = Label(self, text="Use the diary to store your thoughts or narrate how your day went.\nDon't worry, Andy is great at keeping secrets ;]")
+		diary_help.pack(pady=10)
+
+		create_img = ImageTk.PhotoImage(Image.open("create.png").resize((50, 50), Image.ANTIALIAS))
+		create_btn = Button(self, text="Create new entry", image=create_img, anchor="center", compound="left", bd=0, command=lambda: controller.show_frame("DiaryCreateFrame"))
+		create_btn.pack(pady=20)
+
+	def check_records(self):
+		pass
+
+
+class DiaryCreateFrame(Frame):
+	def __init__(self, parent, controller):
+		Frame.__init__(self, parent)
+		# self.grid_rowconfigure(0, weight=1)
+		self.grid_columnconfigure(0, weight=1)
+		self.grid_columnconfigure(1, weight=1)
+		self.grid_columnconfigure(2, weight=1)
+		self.grid_columnconfigure(3, weight=1)
+		self.grid_columnconfigure(4, weight=1)
+
+		global back_img, save_img, mood_1, mood_2, mood_3, mood_4, mood_5
+
+		back_btn = Button(self, text="Back", image=back_img, anchor="center", compound="left", bd=0, padx=-10, command=lambda: controller.show_frame("DiaryFrame"))
+		back_btn.grid(row=0, column=0, columnspan=5, pady=(0, 10))
+
+
+		mood_label = Label(self, text="How did your day go?", anchor="w").grid(row=1, column=0, sticky='nesw', padx=25, columnspan=5)
+
+		self.var = IntVar()
+		mood_1 = ImageTk.PhotoImage(Image.open("mood-1.png").resize((30, 30), Image.ANTIALIAS))
+		mood_2 = ImageTk.PhotoImage(Image.open("mood-2.png").resize((30, 30), Image.ANTIALIAS))
+		mood_3 = ImageTk.PhotoImage(Image.open("mood-3.png").resize((30, 30), Image.ANTIALIAS))
+		mood_4 = ImageTk.PhotoImage(Image.open("mood-4.png").resize((30, 30), Image.ANTIALIAS))
+		mood_5 = ImageTk.PhotoImage(Image.open("mood-5.png").resize((30, 30), Image.ANTIALIAS))
+
+		mood_1_btn = Radiobutton(self, image=mood_1, variable=self.var, value=1, bd=0, anchor="w").grid(row=2, column=0, sticky='nesw', padx=25)
+		mood_2_btn = Radiobutton(self, image=mood_2, variable=self.var, value=2, bd=0, anchor="w").grid(row=2, column=1, sticky='nesw', padx=25)
+		mood_3_btn = Radiobutton(self, image=mood_3, variable=self.var, value=3, bd=0, anchor="w").grid(row=2, column=2, sticky='nesw', padx=25)
+		mood_4_btn = Radiobutton(self, image=mood_4, variable=self.var, value=4, bd=0, anchor="w").grid(row=2, column=3, sticky='nesw', padx=25)
+		mood_5_btn = Radiobutton(self, image=mood_5, variable=self.var, value=5, bd=0, anchor="w").grid(row=2, column=4, sticky='nesw', padx=25)
+
+		# self.title = StringVar()
+		# title_label = Label(self, text="Title", anchor="w").grid(row=3, column=0, sticky='nesw', padx=25, pady=(25, 0), columnspan=5)
+		# title_field = Entry(self, textvariable=self.title).grid(row=4, column=0, sticky='nesw', padx=25, pady=(0,20), columnspan=5)
+
+		entry_label = Label(self, text="Entry", anchor="w").grid(row=5, column=0, sticky='nesw', padx=25, columnspan=5)
+		self.entry_field = ScrolledText(self, height=10)
+		self.entry_field.grid(row=6, column=0, sticky='nesw', padx=25, pady=(0,20), columnspan=5)
+
+		save_img = ImageTk.PhotoImage(Image.open("save.png").resize((25, 25), Image.ANTIALIAS))
+		save_btn = Button(self, text="Save entry", image=save_img, anchor="center", compound="left", bd=0, command=lambda: self.save_entry())
+		save_btn.grid(pady=20, columnspan=5)
+
+	def save_entry(self):
+		print("function works!!")
+
+		print(self.var.get())
+		print(self.entry_field.get("1.0", "end-1c"))
+
+
+
+if __name__ == "__main__":
+	app = Andy()
+	app.mainloop()
