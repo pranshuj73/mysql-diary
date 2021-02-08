0d924c157fd4e615d3bde3749a757d3d510f1aff
volt9801 <pranshuj73@gmail.com>
2021-02-08 23:17:34 +0530

    updated icons for the app to lineal-color versions, minor usability fixes

--- andy.py
+++ andy.py
@@ -24,7 +24,7 @@ class Andy(Tk):
 
 		# creating the frames for the app
 		self.frames = {}
-		for frame_page in (Homescreen, DiaryFrame, DiaryCreateFrame, EntryListFrame, TodoFrame, TaskCreateFrame, TaskEditFrame):
+		for frame_page in (Homescreen, DiaryFrame, DiaryCreateFrame, EntryListFrame, FutureEntryFrame, TodoFrame, TaskCreateFrame, TaskEditFrame):
 			page_name = frame_page.__name__
 			frame = frame_page(parent=container, controller=self)
 			self.frames[page_name] = frame
@@ -64,15 +64,15 @@ class Homescreen(Frame):
 		inner_frame = Frame(self)
 		inner_frame.pack(anchor="center", pady=(30, 0))
 
-		image = Label(inner_frame, image=andy, text="Hii, my name is Andy, your personal diary and to-do app. ^_^", compound="top")
-		image.grid(row=0, column=0, columnspan=2, pady=(0,10))
+		title_label = Label(inner_frame, image=andy, text="Hii, my name is Andy, your personal diary and to-do app. ^_^", compound="top", pady=20)
+		title_label.grid(row=0, column=0, columnspan=2, pady=(0,10))
 
 		diary_img = ImageTk.PhotoImage(Image.open("assets/diary.png").resize((60, 60), Image.ANTIALIAS))
-		diary_btn = Button(inner_frame, text="Diary", image=diary_img, anchor="center", height=100, width=100, compound="top", bd=0, command=lambda: controller.show_frame("DiaryFrame"))
+		diary_btn = Button(inner_frame, text="Diary", image=diary_img, anchor="center", height=100, width=100, compound="top", bd=0, command=lambda: controller.show_frame("DiaryFrame"), cursor="hand1")
 		diary_btn.grid(row=1, column=0)
 
 		todo_img = ImageTk.PhotoImage(Image.open("assets/todo.png").resize((60, 60), Image.ANTIALIAS))
-		todo_btn = Button(inner_frame, text="To-Do", image=todo_img, anchor="center", height=100, width=100, compound="top", bd=0, command=lambda: self.load_todo_frame())
+		todo_btn = Button(inner_frame, text="To-Do", image=todo_img, anchor="center", height=100, width=100, compound="top", bd=0, command=lambda: self.load_todo_frame(), cursor="hand1")
 		todo_btn.grid(row=1, column=1)
 
 	def load_todo_frame(self):
@@ -90,29 +90,33 @@ class DiaryFrame(Frame):
 		global back_img, create_img
 
 		back_img = ImageTk.PhotoImage(Image.open("assets/back.png"))
-		back_btn = Button(self, text="Back to Menu", image=back_img, anchor="center", compound="left", bd=0, padx=-10, command=lambda: controller.show_frame("Homescreen"))
+		back_btn = Button(self, text="Back to Menu", image=back_img, anchor="center", compound="left", bd=0, padx=-10, command=lambda: controller.show_frame("Homescreen"), cursor="hand1")
 		back_btn.pack(pady=(0,20))
 
-		image = Label(self, image=diary_img, text="Diary", compound="left", padx=10)
+		image = Label(self, image=diary_img, text="Diary", compound="left", padx=10, font=('Arial', 16))
 		image.pack()
 
-		diary_help = Label(self, text="Use the diary to store your thoughts or narrate how your day went.\nDon't worry, Andy is great at keeping secrets ;]")
+		diary_help = Label(self, text="Use the diary to store your thoughts or narrate how your day went.\nDon't worry, Andy will never reveal your secrets :]")
 		diary_help.pack(pady=10)
 
-		create_img = ImageTk.PhotoImage(Image.open("assets/create.png").resize((30, 30), Image.ANTIALIAS))
-		create_btn = Button(self, text="Create new entry", image=create_img, anchor="center", compound="left", bd=0, command=lambda: controller.show_frame("DiaryCreateFrame"))
+		create_img = ImageTk.PhotoImage(Image.open("assets/create.png").resize((45, 45), Image.ANTIALIAS))
+		create_btn = Button(self, text="Create new entry", image=create_img, anchor="center", compound="left", bd=0, command=lambda: controller.show_frame("DiaryCreateFrame"), cursor="hand1")
 		create_btn.pack(pady=(10, 20))
 
 		cal = Calendar(self, firstweekday="sunday", showweeknumbers=False, borderwidth=0, date_pattern="yyyy-mm-dd")
 		cal.pack(padx=20, pady=10)
-		print_btn = Button(self, text="Check Entries for the selected date", bd=0, command=lambda: self.check_records(cal.get_date()))
-		print_btn.pack(padx=10, pady=10)
+		check_entries_btn = Button(self, text="Check Entries for the selected date", bd=0, command=lambda: self.check_records(cal.get_date()), cursor="hand1")
+		check_entries_btn.pack(padx=10, pady=10)
 
 	def check_records(self, date):
-		global entries
-		entries = db.fetch_entries(date)  # fetches the entries created on 'date'
-		self.controller.update_entry_list(date)
-		self.controller.show_frame("EntryListFrame")
+		converted_date = datetime.strptime(date, '%Y-%m-%d')
+		if converted_date <= datetime.today():
+			global entries
+			entries = db.fetch_entries(date)  # fetches the entries created on 'date'
+			self.controller.update_entry_list(date)
+			self.controller.show_frame("EntryListFrame")
+		else:
+			self.controller.show_frame("FutureEntryFrame")
 
 
 class DiaryCreateFrame(Frame):
@@ -129,7 +133,7 @@ class DiaryCreateFrame(Frame):
 
 		global back_img, save_img, mood_1, mood_2, mood_3, mood_4, mood_5
 
-		back_btn = Button(self, text="Back", image=back_img, anchor="center", compound="left", bd=0, padx=-10, command=lambda: controller.show_frame("DiaryFrame"))
+		back_btn = Button(self, text="Back", image=back_img, anchor="center", compound="left", bd=0, padx=-10, command=lambda: controller.show_frame("DiaryFrame"), cursor="hand1")
 		back_btn.grid(row=0, column=0, columnspan=5, pady=(0, 20))
 
 		date_label = Label(self, font=('calibri', 40), anchor="e")
@@ -175,8 +179,8 @@ class DiaryCreateFrame(Frame):
 		self.entry_field = ScrolledText(self, height=10, width=10)
 		self.entry_field.grid(row=7, column=0, sticky='nesw', padx=25, pady=(0,20), columnspan=5)
 
-		save_img = ImageTk.PhotoImage(Image.open("assets/save.png").resize((25, 25), Image.ANTIALIAS))
-		save_btn = Button(self, text="Save entry", image=save_img, anchor="center", compound="left", bd=0, command=lambda: self.save_entry())
+		save_img = ImageTk.PhotoImage(Image.open("assets/save.png").resize((40, 40), Image.ANTIALIAS))
+		save_btn = Button(self, text="Save your new entry", image=save_img, anchor="center", compound="left", bd=0, command=lambda: self.save_entry(), cursor="hand1")
 		save_btn.grid(row=8, column=0, sticky='nesw', pady=20, columnspan=5)
 
 	def save_entry(self):
@@ -212,7 +216,11 @@ class EntryListFrame(Frame):
 		self.controller = controller
 		self.pack_propagate(False)
 
-		back_btn = Button(self, text="Back", image=back_img, anchor="center", compound="left", bd=0, padx=-10, command=lambda: self.show_prev_frame())
+		global no_entry_img
+
+		no_entry_img = ImageTk.PhotoImage(Image.open("assets/no_entry.png").resize((128, 128), Image.ANTIALIAS))
+
+		back_btn = Button(self, text="Back", image=back_img, anchor="center", compound="left", bd=0, padx=-10, command=lambda: self.show_prev_frame(), cursor="hand1")
 		back_btn.pack(pady=(0, 20))
 
 	def load_content(self, date):
@@ -232,18 +240,21 @@ class EntryListFrame(Frame):
 		year, month, day = self.date.split('-')
 		date_label = Label(inner_frame, text=f'Date: {day}-{month}-{year}', anchor="center", bd=0, padx=10, pady=10, font=('calibri', 16)).pack()
 
-		create_btn = Button(inner_frame, text="Add another entry?", image=create_img, anchor="center", compound="left", bd=0, command=lambda: self.controller.show_frame("DiaryCreateFrame")).pack(pady=10)
+		if  entries:
+			# creating labels for each entry or showing "No entries were created for this day" if entries don't exist
+			emoticons = {1: mood_1, 2: mood_2, 3: mood_3, 4: mood_4, 5: mood_5}
+			for entry in entries:
+				mood, content, created_at, edited_at = entry
+				time_created_label = Label(inner_frame, text=f"[created at {created_at.strftime('%H:%M')}]", image=emoticons[mood], anchor="center", compound="left", bd=0, padx=10).pack(padx=10, pady=(20,0))
+				entry_label = Label(inner_frame, text=content, anchor="center", bd=0, wraplength=self.winfo_width()-50).pack(padx=10, pady=(0,20))
 
-		# creating labels for each entry or showing "No entries were created for this day" if entries don't exist
-		emoticons = {1: mood_1, 2: mood_2, 3: mood_3, 4: mood_4, 5: mood_5}
-		for entry in entries:
-			mood, content, created_at, edited_at = entry
-			time_created_label = Label(inner_frame, text=f"[created at {created_at.strftime('%H:%M')}]", image=emoticons[mood], anchor="center", compound="left", bd=0, padx=10).pack(padx=10, pady=(20,0))
-			entry_label = Label(inner_frame, text=content, anchor="center", bd=0, wraplength=self.winfo_width()-50).pack(padx=10, pady=(0,20))
+			create_btn = Button(inner_frame, text="Add another entry?", image=create_img, anchor="center", compound="left", bd=0, command=lambda: self.controller.show_frame("DiaryCreateFrame"), cursor="hand1").pack(pady=10)
 
-		if not entries:
-			empty_msg = Label(inner_frame, text="No entries were created for this day", image=emoticons[4], anchor="center", compound="top", bd=0, padx=10, pady=10, wraplength=self.winfo_width()-50)
+		elif not entries:
+			empty_msg = Label(inner_frame, text="No entries were created for this day", image=no_entry_img, anchor="center", compound="top", bd=0, padx=10, pady=10, wraplength=self.winfo_width()-50)
 			empty_msg.pack(pady=50)
+
+			create_btn = Button(inner_frame, text="Why not add an entry?", image=create_img, anchor="center", compound="left", bd=0, command=lambda: self.controller.show_frame("DiaryCreateFrame"), cursor="hand1").pack(pady=10)
 
 		self.canvas.update_idletasks()
 		self.canvas.configure(scrollregion=self.canvas.bbox('all'), yscrollcommand=self.scrollbar.set)
@@ -269,6 +280,24 @@ class EntryListFrame(Frame):
 			self.scrollbar.destroy()
 
 
+class FutureEntryFrame(Frame):
+	'''Method to view all entries created by user on a particular date'''
+	def __init__(self, parent, controller):
+		Frame.__init__(self, parent)
+		self.controller = controller
+		self.pack_propagate(False)
+
+		global crying_img
+
+		crying_img = ImageTk.PhotoImage(Image.open("assets/crying.png").resize((128, 128), Image.ANTIALIAS))
+
+		back_btn = Button(self, text="Back", image=back_img, anchor="center", compound="left", bd=0, padx=-10, command=lambda: self.controller.show_frame("DiaryFrame"), cursor="hand1")
+		back_btn.pack(pady=(0, 20))
+
+		cant_add_entries_msg = Label(self, text="Unfortunately, only those with time-travelling abilities\ncan see diary entries for the future!", image=crying_img, anchor="center", compound="top", bd=0, padx=10, pady=30, font=('Arial', 14))
+		cant_add_entries_msg.pack(pady=50)
+
+
 class TodoFrame(Frame):
 	'''Todo Frame for users to create aand view tasks created'''
 	def __init__(self, parent, controller):
@@ -278,17 +307,17 @@ class TodoFrame(Frame):
 
 		global add_task_img
 
-		back_btn = Button(self, text="Back to Menu", image=back_img, anchor="center", compound="left", bd=0, padx=-10, command=lambda: controller.show_frame("Homescreen"))
+		back_btn = Button(self, text="Back to Menu", image=back_img, anchor="center", compound="left", bd=0, padx=-10, command=lambda: controller.show_frame("Homescreen"), cursor="hand1")
 		back_btn.pack(pady=(0,20))
 
-		image = Label(self, image=todo_img, text="Tasks", compound="left", padx=10)
-		image.pack()
+		title_label = Label(self, image=todo_img, text="Tasks", compound="left", padx=10, font=('Arial', 16))
+		title_label.pack()
 
 		todo_help = Label(self, text="Add your daily tasks/chores here and\ncheck them off when you've completed them.")
 		todo_help.pack(pady=10)
 
-		add_task_img = ImageTk.PhotoImage(Image.open("assets/todo_add_new.png").resize((30, 30), Image.ANTIALIAS))
-		add_task_btn = Button(self, text="Add new task", image=add_task_img, anchor="center", compound="left", bd=0, command=lambda: controller.show_frame("TaskCreateFrame"))
+		add_task_img = ImageTk.PhotoImage(Image.open("assets/todo_add_new.png").resize((45, 45), Image.ANTIALIAS))
+		add_task_btn = Button(self, text="Add new task", image=add_task_img, anchor="center", compound="left", bd=0, command=lambda: controller.show_frame("TaskCreateFrame"), cursor="hand1")
 		add_task_btn.pack()
 
 	def load_tasks(self):
@@ -312,7 +341,7 @@ class TodoFrame(Frame):
 			# creating button for each task that maps to the edit task frame
 			for task in self.incomplete_tasks:
 				title, description, priority, status = task
-				task_btn = Button(task_frame, text=title, bg=priority_colors[priority-1], bd=0, width=25, wraplength=400, justify="left", anchor="w", pady=5, command=lambda task=task: self.get_task(task))
+				task_btn = Button(task_frame, text=title, bg=priority_colors[priority-1], bd=0, width=25, wraplength=400, justify="left", anchor="w", pady=5, command=lambda task=task: self.get_task(task), cursor="hand1")
 				task_btn.pack(expand=True, padx=(50,0))
 
 			self.completed_tasks = db.fetch_completed_tasks()
@@ -323,13 +352,13 @@ class TodoFrame(Frame):
 				completed_task_label = Label(task_frame, text="Completed Tasks", font=('calibri', 16)).pack(padx=(50,0), pady=10, anchor="center")
 				for task in self.completed_tasks:
 					title, description, priority, status = task
-					task_btn = Button(task_frame, text=title, image=done_icon, compound="left", bd=0, width=200, wraplength=400, justify="left", anchor="w", pady=5, command=lambda task=task: self.get_task(task))
+					task_btn = Button(task_frame, text=title, image=done_icon, compound="left", bd=0, width=200, wraplength=400, justify="left", anchor="w", pady=5, command=lambda task=task: self.get_task(task), cursor="hand1")
 					task_btn.pack(expand=True, padx=(50,0))
 
 		else:
 			global no_task_img
-			no_task_img = ImageTk.PhotoImage(Image.open("assets/no_task.png").resize((340, 160), Image.ANTIALIAS))
-			all_complete = Label(task_frame, text="You're all caught up!", image=no_task_img, compound="top", font=('calibri', 16)).pack(padx=(50,0), pady=10)
+			no_task_img = ImageTk.PhotoImage(Image.open("assets/no_task.png").resize((128, 128), Image.ANTIALIAS))
+			all_complete = Label(task_frame, text="You're all caught up!", image=no_task_img, compound="top", font=('Arial', 16), pady=20).pack(padx=(50,0), pady=10)
 
 		self.canvas.update_idletasks()
 		self.canvas.configure(scrollregion=self.canvas.bbox('all'), yscrollcommand=self.scrollbar.set)
@@ -361,7 +390,7 @@ class TaskCreateFrame(Frame):
 		self.grid_columnconfigure(0, weight=1)
 		self.grid_columnconfigure(1, weight=1)
 
-		back_btn = Button(self, text="Back", image=back_img, anchor="center", compound="left", bd=0, padx=-10, command=lambda: self.load_todo_frame())
+		back_btn = Button(self, text="Back", image=back_img, anchor="center", compound="left", bd=0, padx=-10, command=lambda: self.load_todo_frame(), cursor="hand1")
 		back_btn.grid(row=0, column=0, columnspan=2)
 		instruction_label = Label(self, text="Add new task here.", font=('calibri', 16)).grid(row=1, column=0, padx=25, columnspan=2)
 
@@ -379,17 +408,17 @@ class TaskCreateFrame(Frame):
 		self.priority_label.grid(row=6, column=0, sticky='nesw', padx=25, columnspan=2)
 
 		self.priority_value = 0
-		self.priority_1_btn = Button(self, text="Important & Urgent\n[Do it now]", bd=0, bg="#80FDFF", fg="gray15", highlightbackground="#80FDFF", disabledforeground="black", pady=10, padx=5, command=lambda: self.disable_self(1))
-		self.priority_2_btn = Button(self, text="Important but not Urgent\n[Schedule a time to do it]", bd=0, bg="#90EE90", fg="gray15", highlightbackground="#90EE90", disabledforeground="black", pady=10, padx=5, command=lambda: self.disable_self(2))
-		self.priority_3_btn = Button(self, text="Urgent but not Important\n[What can do it for you?]", bd=0, bg="#FF9380", fg="gray15", highlightbackground="#FF9380", disabledforeground="black", pady=10, padx=5, command=lambda: self.disable_self(3))
-		self.priority_4_btn = Button(self, text="Neither Important nor Urgent\n[Eliminate it]", bd=0, bg="#A5BCD9", fg="gray15", highlightbackground="#A5BCD9", disabledforeground="black", pady=10, padx=5, command=lambda: self.disable_self(4))
+		self.priority_1_btn = Button(self, text="Important & Urgent\n[Do it now]", bd=0, bg="#80FDFF", fg="gray15", highlightbackground="#80FDFF", disabledforeground="black", pady=10, padx=5, command=lambda: self.disable_self(1), cursor="hand1")
+		self.priority_2_btn = Button(self, text="Important but not Urgent\n[Schedule a time to do it]", bd=0, bg="#90EE90", fg="gray15", highlightbackground="#90EE90", disabledforeground="black", pady=10, padx=5, command=lambda: self.disable_self(2), cursor="hand1")
+		self.priority_3_btn = Button(self, text="Urgent but not Important\n[What can do it for you?]", bd=0, bg="#FF9380", fg="gray15", highlightbackground="#FF9380", disabledforeground="black", pady=10, padx=5, command=lambda: self.disable_self(3), cursor="hand1")
+		self.priority_4_btn = Button(self, text="Neither Important nor Urgent\n[Eliminate it]", bd=0, bg="#A5BCD9", fg="gray15", highlightbackground="#A5BCD9", disabledforeground="black", pady=10, padx=5, command=lambda: self.disable_self(4), cursor="hand1")
 
 		self.priority_1_btn.grid(row=7, column=0, sticky='nsew', padx=25, pady=5)
 		self.priority_2_btn.grid(row=7, column=1, sticky='nsew', padx=25, pady=5)
 		self.priority_3_btn.grid(row=8, column=0, sticky='nsew', padx=25, pady=5)
 		self.priority_4_btn.grid(row=8, column=1, sticky='nsew', padx=25, pady=5)
 
-		save_btn = Button(self, text="Save task", image=save_img, anchor="center", compound="left", bd=0, command=lambda: self.save_entry())
+		save_btn = Button(self, text="Save your task", image=save_img, anchor="center", compound="left", bd=0, command=lambda: self.save_entry(), cursor="hand1")
 		save_btn.grid(row=9, column=0, sticky='nesw', padx=25, columnspan=2, pady=10)
 
 	def disable_self(self, pos):
@@ -438,18 +467,18 @@ class TaskEditFrame(Frame):
 		self.grid_columnconfigure(1, weight=1)
 
 		global delete_icon, completed_icon, incomplete_icon
-		delete_icon = ImageTk.PhotoImage(Image.open("assets/delete.png").resize((20, 20), Image.ANTIALIAS))
-		completed_icon = ImageTk.PhotoImage(Image.open("assets/completed.png").resize((20, 20), Image.ANTIALIAS))	
-		incomplete_icon = ImageTk.PhotoImage(Image.open("assets/incomplete.png").resize((20, 20), Image.ANTIALIAS))
+		delete_icon = ImageTk.PhotoImage(Image.open("assets/delete.png").resize((45, 45), Image.ANTIALIAS))
+		completed_icon = ImageTk.PhotoImage(Image.open("assets/completed.png").resize((45, 45), Image.ANTIALIAS))	
+		incomplete_icon = ImageTk.PhotoImage(Image.open("assets/incomplete.png").resize((45, 45), Image.ANTIALIAS))
 
-		back_btn = Button(self, text="Back", image=back_img, anchor="center", compound="left", bd=0, padx=-10, command=lambda: self.load_todo_frame())
+		back_btn = Button(self, text="Back", image=back_img, anchor="center", compound="left", bd=0, padx=-10, command=lambda: self.load_todo_frame(), cursor="hand1")
 		back_btn.grid(row=0, column=0, columnspan=2, pady=(0,20))
 
 		self.title_label = Label(self, font=('calibri', 16), pady=5)
 		self.description_label = Label(self, font=('calibri', 12), pady=5)
 		self.priority_label = Label(self, font=('calibri', 8, 'bold'), pady=10)
-		self.change_status_btn = Button(self, bd=0)
-		self.delete_btn = Button(self, text="Delete", bd=0, image=delete_icon, compound="left", anchor="center", bg="#ff5252")
+		self.change_status_btn = Button(self, bd=0, cursor="hand1")
+		self.delete_btn = Button(self, text="Delete", bd=0, image=delete_icon, compound="left", anchor="center", cursor="hand1")
 
 		self.title_label.grid(row=1, column=0, columnspan=2)
 		self.description_label.grid(row=2, column=0, columnspan=2)
--- init_db.py
+++ init_db.py
@@ -4,20 +4,26 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-db = connector.connect(host="localhost",
-	user=os.environ.get('MYSQL_USERNAME'),
-	password=os.environ.get('MYSQL_PASSWORD')
+db = connector.connect(
+    host="localhost",
+    user=os.environ.get("MYSQL_USERNAME"),
+    password=os.environ.get("MYSQL_PASSWORD"),
 )
 cur = db.cursor()
 cur.execute("CREATE DATABASE andy;")
 print("Database created successfully!")
 
-db = connector.connect(host="localhost",
-	user=os.environ.get('MYSQL_USERNAME'),
-	password=os.environ.get('MYSQL_PASSWORD'),
-	database="andy"
+db = connector.connect(
+    host="localhost",
+    user=os.environ.get("MYSQL_USERNAME"),
+    password=os.environ.get("MYSQL_PASSWORD"),
+    database="andy",
 )
 cur = db.cursor()
-cur.execute("CREATE TABLE diary(mood int, entry varchar(5000), dt datetime, last_edited datetime);")
-cur.execute("CREATE TABLE todo(title varchar(200), description varchar(5000), priority int, status boolean);")
+cur.execute(
+    "CREATE TABLE diary(mood int, entry varchar(5000), dt datetime, last_edited datetime);"
+)
+cur.execute(
+    "CREATE TABLE todo(title varchar(200), description varchar(5000), priority int, status boolean);"
+)
 print("Tables created successfully!")
